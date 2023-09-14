dc741ee34805bcd8d3ff6f0bd5d8512579560ddc
root <root@SkyDesk.localdomain>
2023-09-14 11:00:52 +0000

    script change

--- profile.d/aliases.sh
+++ profile.d/aliases.sh
@@ -67,15 +67,80 @@ hun() {
 
 alias lnms="kubectl exec --namespace=librenms --stdin --tty lnms-dispatcher-0 -- /usr/bin/lnms"
 
+# nms() {
+#     local LNMS_DIR="${LNMS_DIR:-/data}"
+
+#     # Store the current directory
+#     case "$1" in
+#         "edit")
+#             if [ $? -eq 0 ]; then
+#                 vim -f "$LNMS_DIR/lnms-config.yaml" || return 1
+#                 helm upgrade librenms "$LNMS_DIR/chartLibreNMS-Helm/" -f "$LNMS_DIR/lnms-config.yaml" || return 1
+#             else
+#                 echo "Editing configuration failed. SUDO may be needed"
+#             fi
+#             ;;
+#         "help")
+#             cat "$LNMS_DIR/installer/doc/ailiases.txt" || return 1
+#             ;;
+#         "install")
+#             function LibreClusterInstall() {
+#                 helm install librenms "$LNMS_DIR/chart/LibreNMS-Helm/" -f "$LNMS_DIR/lnms-config.yaml"
+#             }
+
+#             # Function to add LibreNMS IP to SNMP monitoring
+#             function LibreSNMPadd() {
+#                 ip_eth=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+#                 ip_ens=$(/sbin/ip -o -4 addr list ens160 | awk '{print $4}' | cut -d/ -f1)
+
+#                 if [ "$ip_ens" ]; then
+#                     echo "Adding $ip_ens to SNMP monitoring..."
+#                     kubectl exec --namespace=librenms --stdin --tty lnms-dispatcher-0 -- /usr/bin/lnms device:add -2 -c locallibremon -r 1161 -d LibreNMS "$ip_ens"
+#                 elif [ "$ip_eth" ]; then
+#                     echo "Adding $ip_eth to SNMP monitoring..."
+#                     kubectl exec --namespace=librenms --stdin --tty lnms-dispatcher-0 -- /usr/bin/lnms device:add -2 -c locallibremon -r 1161 -d LibreNMS "$ip_eth"
+#                 else
+#                     echo "No IP address could be found for SNMP monitoring."
+#                 fi
+#             }
+
+#             nano -f "$LNMS_DIR/lnms-config.yaml"
+
+#             echo "################## Starting LibreNMS Cluster Installation ##################"
+#             echo "To monitor the process you can run 'sudo k9s' in another terminal."
+#             echo ""
+#             LibreClusterInstall
+#             LibreSNMPadd
+#             echo "################## Cluster Starup Complete ##################"
+#             ;;
+#         "update")
+#             helm upgrade librenms "$LNMS_DIR/chartLibreNMS-Helm/" -f "$LNMS_DIR/lnms-config.yaml" || return 1
+#             ;;
+#         "map")
+#             local pod_name=$(kubectl get pods -n librenms | grep lnms-app | awk '{print $1}')
+#             if [ -n "$pod_name" ]; then
+#                 kubectl exec -it "$pod_name" --namespace=librenms -- php /opt/librenms/html/plugins/Weathermap/map-poller.php
+#             else
+#                 echo "Error: LibreNMS pod not found."
+#             fi
+#             ;;
+#         *)
+#             echo "Usage: $0 {edit|update|map|help}"
+#             return 1
+#             ;;
+#     esac
+# }
+
+
 nms() {
     local LNMS_DIR="${LNMS_DIR:-/data}"
 
     # Store the current directory
     case "$1" in
         "edit")
-            if [ $? -eq 0 ]; then
+            if [ "$?" -eq 0 ]; then
                 vim -f "$LNMS_DIR/lnms-config.yaml" || return 1
-                helm upgrade librenms "$LNMS_DIR/chartLibreNMS-Helm/" -f "$LNMS_DIR/lnms-config.yaml" || return 1
+                helm upgrade librenms "$LNMS_DIR/chart/LibreNMS-Helm/" -f "$LNMS_DIR/lnms-config.yaml" || return 1
             else
                 echo "Editing configuration failed. SUDO may be needed"
             fi
@@ -83,8 +148,38 @@ nms() {
         "help")
             cat "$LNMS_DIR/installer/doc/ailiases.txt" || return 1
             ;;
+        "install")
+            LibreClusterInstall() {
+                helm install librenms "$LNMS_DIR/chart/LibreNMS-Helm/" -f "$LNMS_DIR/lnms-config.yaml"
+            }
+
+            # Function to add LibreNMS IP to SNMP monitoring
+            LibreSNMPadd() {
+                local ip_eth=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+                local ip_ens=$(/sbin/ip -o -4 addr list ens160 | awk '{print $4}' | cut -d/ -f1)
+
+                if [ "$ip_ens" ]; then
+                    echo "Adding $ip_ens to SNMP monitoring..."
+                    kubectl exec --namespace=librenms --stdin --tty lnms-dispatcher-0 -- /usr/bin/lnms device:add -2 -c locallibremon -r 1161 -d LibreNMS "$ip_ens"
+                elif [ "$ip_eth" ]; then
+                    echo "Adding $ip_eth to SNMP monitoring..."
+                    kubectl exec --namespace=librenms --stdin --tty lnms-dispatcher-0 -- /usr/bin/lnms device:add -2 -c locallibremon -r 1161 -d LibreNMS "$ip_eth"
+                else
+                    echo "No IP address could be found for SNMP monitoring."
+                fi
+            }
+
+            nano -f "$LNMS_DIR/lnms-config.yaml"
+
+            echo "################## Starting LibreNMS Cluster Installation ##################"
+            echo "To monitor the process, you can run 'sudo k9s' in another terminal."
+            echo ""
+            LibreClusterInstall
+            LibreSNMPadd
+            echo "################## Cluster Startup Complete ##################"
+            ;;
         "update")
-            helm upgrade librenms "$LNMS_DIR/chartLibreNMS-Helm/" -f "$LNMS_DIR/lnms-config.yaml" || return 1
+            helm upgrade librenms "$LNMS_DIR/chart/LibreNMS-Helm/" -f "$LNMS_DIR/lnms-config.yaml" || return 1
             ;;
         "map")
             local pod_name=$(kubectl get pods -n librenms | grep lnms-app | awk '{print $1}')
