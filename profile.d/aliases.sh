2a29e17c61b09c65f931e38b9a7c7853018e7658
root <root@SkyDesk.localdomain>
2023-11-16 14:36:24 +0000

    adding nms functions

--- profile.d/aliases.sh
+++ profile.d/aliases.sh
@@ -76,17 +76,6 @@ nms() {
     local KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
     case "$action" in
-        "edit")  
-
-            echo "Editing LibreNMS configuration..."
-            if [ "$?" -eq 0 ]; then
-                vim -f "$LNMS_DIR/lnms-config.yaml" || return 1
-                helm upgrade librenms "$LNMS_DIR/chart/LibreNMS-Helm/" -f "$LNMS_DIR/lnms-config.yaml" || return 1
-            else
-                echo "Edit failed. Check permissions."
-            fi
-            ;;
-
         "start")
 
             if kubectl get deployment librenms 2>/dev/null; then
@@ -117,12 +106,46 @@ nms() {
                 echo "Installation failed, check configuration."
             fi
             ;;
+        
+        "stop")
+
+            echo "Stopping LibreNMS..."
+            helm uninstall librenms || return 1
+            ;;
+
+        "edit")  
+
+            echo "Editing LibreNMS configuration..."
+            if [ "$?" -eq 0 ]; then
+                vim -f "$LNMS_DIR/lnms-config.yaml" || return 1
+                helm upgrade librenms "$LNMS_DIR/chart/LibreNMS-Helm/" -f "$LNMS_DIR/lnms-config.yaml" || return 1
+            else
+                echo "Edit failed. Check permissions."
+            fi
+            ;;
 
         "update")
 
             echo "Upgrading LibreNMS installation..."  
             helm upgrade librenms "$LNMS_DIR/chart/LibreNMS-Helm/" -f "$LNMS_DIR/lnms-config.yaml" || return 1
             ;;
+
+        "status")
+
+            echo "Checking LibreNMS status..."
+            local pod_name=$(kubectl get pods -n librenms | grep lnms-app | awk '{print $1}')
+
+            if [ -n "$pod_name" ]; then
+                kubectl describe pod "$pod_name" -n librenms
+            else
+                echo "Error: LibreNMS pod not found."
+            fi
+
+            echo "Starting k9s dashboard..."
+            k9s
+            ;;
+
+
 
         "map")
 
@@ -136,15 +159,28 @@ nms() {
             fi
             ;;
 
+         "cert")
+
+            echo "Inserting TLS secret manually..."
+
+            if [ -z "$2" ] || [ -z "$3" ]; then
+                echo "Cert and key files required: "
+                echo "nms cert /path/to/cert.pem /path/to/cert.key"  
+                return 1  
+            fi
+
+            kubectl create secret tls https --cert="$2" --key="$3"
+            ;;
+
         "help")
 
             echo "Displaying help..."
-            cat "$LNMS_DIR/installer/doc/ailiases.txt" || return 1  
+            cat "$LNMS_DIR/installer/doc/nmsinfo.txt" || return 1  
             ;;
 
         *)
 
-            echo "Usage: $0 {edit|update|start|map|help}"
+            echo "Usage: $0 {start|stop|edit|update|status|update|map|help}"
             return 1
             ;;
     esac
