30bfa7670116a8cd3386c3cfb881088b1ae81261
Stebbi <stefan.sigurdsson@sensa.is>
2026-09-23 09:19:23 +0000

    x

--- profile.d/helm-shorthand.sh
+++ profile.d/helm-shorthand.sh
@@ -4,29 +4,29 @@ alias hl="helm list"
 alias hall="helm list --all-namespaces"
 
 hin() {
-    if [ "$2" = "" ]; then
-            echo -e "Error, please provide a release name, chart and value file"
-    elif [ "$3" = "" ]; then
-            helm install $1 $2 
-    else 
-            helm install $1 $2 -f $3 
-    fi
+        if [ "$2" = "" ]; then
+                echo -e "Error, please provide a release name, chart and value file"
+        elif [ "$3" = "" ]; then
+                helm install $1 $2
+        else 
+        helm install $1 $2 -f $3 
+        fi
 }
 
 hup()  {
-    if [ "$2" = "" ]; then
-            echo -e "Error, please provide a release name, chart and value file"
-    elif [ "$3" = "" ]; then
-            helm upgrade $1 $2 
-    else 
-            helm upgrade $1 $2 -f $3 
-    fi
+        if [ "$2" = "" ]; then
+                echo -e "Error, please provide a release name, chart and value file"
+        elif [ "$3" = "" ]; then
+                helm upgrade $1 $2 
+        else 
+                helm upgrade $1 $2 -f $3 
+        fi
 }
 
 hun() {
-    if [ "$1" != "" ]; then
-            helm uninstall $1
-    else
-            echo -e "Error, please provide a release name"
-    fi
+        if [ "$1" != "" ]; then
+                helm uninstall $1
+        else
+                echo -e "Error, please provide a release name"
+        fi
 }
